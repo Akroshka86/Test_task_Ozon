c10e1aa3a705f532010ad0e303a30d751b8c4159
Michail Vavilov <Manchester02016@rambler.ru>
2025-11-10 13:07:25 +0300

    feat: add class ProgressBar

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const rings = document.querySelector('.progress-container');
-const value = +rings.dataset.value;
-const circle = rings.querySelector('.progress-svg-value');
-const radius = circle.r.baseVal.value;
-const circleLength = 2 * Math.PI * radius;
-const input = document.getElementById('input-value')
+class ProgressBar{
+    constructor(progress_container, input_value){
+        this.rings = document.querySelector(progress_container);
+        this.circle = this.rings.querySelector('.progress-svg-value');
+        this.radius = this.circle.r.baseVal.value;
+        this.circleLength = 2 * Math.PI * this.radius;
+        this.input = document.getElementById(input_value)
 
-circle.style.strokeDasharray = circleLength;
-circle.style.strokeDashoffset = circleLength;
-rings.dataset.value = 0;
+        this.circle.style.strokeDasharray = this.circleLength;
+        this.circle.style.strokeDashoffset = this.circleLength;
+        this.rings.dataset.value = 0;
 
-function updateProgressBar(value){
-    circle.style.strokeDashoffset = circleLength * (1 - value / 100);
-    rings.dataset.value = value;
+        this.input.addEventListener('input', () => {
+            this.updateProgressBar(+this.input.value);
+        });
+    }
+
+    updateProgressBar(value){
+        this.circle.style.strokeDashoffset = this.circleLength * (1 - value / 100);
+        this.rings.dataset.value = value;
+    }
 }
 
-
-input.addEventListener('input', function(){
-    updateProgressBar(+this.value);
-})
-
+const progressBar = new ProgressBar('.progress-container', 'input-value');
