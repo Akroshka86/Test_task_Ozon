797006f159f466c3faf1c189ad42a4c438c659ae
Michail Vavilov <Manchester02016@rambler.ru>
2025-11-10 12:53:29 +0300

    feat: add input-value

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const value = +rings.dataset.value;
 const circle = rings.querySelector('.progress-svg-value');
 const radius = circle.r.baseVal.value;
 const circleLength = 2 * Math.PI * radius;
+const input = document.getElementById('input-value')
 
 circle.style.strokeDasharray = circleLength;
-circle.style.strokeDashoffset = circleLength * (1 - value / 100);
+circle.style.strokeDashoffset = circleLength;
+rings.dataset.value = 0;
+
+function updateProgressBar(value){
+    circle.style.strokeDashoffset = circleLength * (1 - value / 100);
+    rings.dataset.value = value;
+}
+
+
+input.addEventListener('input', function(){
+    updateProgressBar(+this.value);
+})
+
