3182a70f323515f720cfc804ed47345e86561bac
Michail Vavilov <Manchester02016@rambler.ru>
2025-11-10 13:39:49 +0300

    feat: add input-checkbox-hidden

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 class ProgressBar{
-    constructor(progress_container, input_value, checkboxSelector){
+    constructor(progress_container, input_value, rotateCheckbox, hidenCheckbox){
         this.rings = document.querySelector(progress_container);
         this.circle = this.rings.querySelector('.progress-svg-value');
         this.radius = this.circle.r.baseVal.value;
         this.circleLength = 2 * Math.PI * this.radius;
         this.input = document.getElementById(input_value);
-        this.checkbox = document.getElementById(checkboxSelector);
+        this.rotateCheckbox = document.getElementById(rotateCheckbox);
+        this.hidenCheckbox = document.getElementById(hidenCheckbox);
         this.svg = this.rings.querySelector('.progress-svg');
 
         this.circle.style.strokeDasharray = this.circleLength;
@@ -16,8 +17,12 @@ class ProgressBar{
             this.updateProgressBar(+this.input.value);
         });
 
-        this.checkbox.addEventListener('change', () => {
-            this.toggleAnimation(+this.checkbox.checked);
+        this.rotateCheckbox.addEventListener('change', () => {
+            this.toggleAnimation(+this.rotateCheckbox.checked);
+        });
+
+        this.hidenCheckbox.addEventListener('change', () => {
+            this.toggleHidden(+this.hidenCheckbox.checked);
         });
     }
 
@@ -29,6 +34,10 @@ class ProgressBar{
     toggleAnimation(isAnimated){
         this.svg.style.animation = isAnimated ? `rotate 2s linear infinite` : `none`;
     }
+
+    toggleHidden(isHidden){
+        this.rings.style.display = isHidden ? `none` : `block`;
+    }
 }
 
-const progressBar = new ProgressBar('.progress-container', 'input-value', 'input-checkbox');
+const progressBar = new ProgressBar('.progress-container', 'input-value', 'input-checkbox', 'input-checkbox-hidden');
